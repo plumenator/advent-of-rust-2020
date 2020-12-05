4541610f164f9000ef9ace7697089d9872f025dc
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2020-12-05 22:09:56 +0800

    Solve Part 2

--- src/day5.rs
+++ src/day5.rs
@@ -96,6 +96,37 @@ fn reduce_h(chars: &str, lchar: char, min: usize, max: usize) -> (usize, usize) 
     }
 }
 
+// --- Part Two ---
+
+// Ding! The "fasten seat belt" signs have turned on. Time to find
+// your seat.
+
+// It's a completely full flight, so your seat should be the only
+// missing boarding pass in your list. However, there's a catch: some
+// of the seats at the very front and back of the plane don't exist on
+// this aircraft, so they'll be missing from your list as well.
+
+// Your seat wasn't at the very front or back, though; the seats with
+// IDs +1 and -1 from yours will be in your list.
+
+// What is the ID of your seat?
+
+pub fn part2() -> usize {
+    let file = File::open(Path::new("day5-input.txt")).expect("open");
+    let mut seat_ids: Vec<_> = io::BufReader::new(file)
+        .lines()
+        .map(|pass| seat_id(&pass.expect("pass")))
+        .collect();
+    seat_ids.sort();
+    let (f, _) = seat_ids[..seat_ids.len()]
+        .iter()
+        .zip(seat_ids[1..].iter())
+        .filter(|(f, s)| *s - *f > 1)
+        .next()
+        .unwrap();
+    f + 1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -111,5 +142,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(828, part1())
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(565, part2())
     }
 }
